c77a1b20c06d49df583c18dcebd25023ac5238e9
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-04-17 16:56:27 +0200

    Lek med litt DI

---
+++ src/main/java/com/nanodami/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,13 @@
+package com.nanodami.sfgdi.controllers;
+
+import com.nanodami.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+
+    public GreetingService greetingService;
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+
+}
