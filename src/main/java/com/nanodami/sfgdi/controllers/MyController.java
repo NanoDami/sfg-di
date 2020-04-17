c8a2ff9a96875969653b8056e0cdd5fc1bf6e180
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-04-17 16:19:46 +0200

    Added a tiny controller

---
+++ src/main/java/com/nanodami/sfgdi/controllers/MyController.java
@@ -0,0 +1,14 @@
+package com.nanodami.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello()  {
+        System.out.println("Hello");
+
+        return "Hi";
+    }
+
+}
