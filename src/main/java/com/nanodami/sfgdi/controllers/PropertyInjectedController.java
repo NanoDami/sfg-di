e399bb07338128e1f5b2f4c0046ffd05925402f7
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-04-20 11:08:18 +0200

    Satte opp service til å kunne tas i bruk av spring

--- src/main/java/com/nanodami/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/com/nanodami/sfgdi/controllers/PropertyInjectedController.java
@@ -1,11 +1,13 @@
 package com.nanodami.sfgdi.controllers;
 
 import com.nanodami.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting() {
