71b86dc7e41d32fe3859d9debd15c8244a8c3c10
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-04-20 11:24:01 +0200

    La til Autowired på servicen vi konsumerer.

--- src/main/java/com/nanodami/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/com/nanodami/sfgdi/controllers/SetterInjectedController.java
@@ -1,6 +1,7 @@
 package com.nanodami.sfgdi.controllers;
 
 import com.nanodami.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -8,6 +9,7 @@ public class SetterInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
