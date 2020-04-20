3233bed7cbcffd27500ecc0062d6bb0fc8db6340
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-04-20 13:14:42 +0200

    Added a lacking @Controller

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,10 +1,12 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
     private final PetService petService;
