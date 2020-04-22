497aa00308219d9e823c27b494557fb0ab17ec72
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-04-22 13:38:24 +0200

    Learned how to set up and utilize a property source

--- src/main/java/com/nanodami/sfgdi/SfgDiApplication.java
+++ src/main/java/com/nanodami/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package com.nanodami.sfgdi;
 
 import com.nanodami.sfgdi.controllers.*;
+import com.nanodami.sfgdi.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,31 +15,14 @@ public class SfgDiApplication {
 
 		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
 
-		System.out.println(i18nController.sayHello());
-
 		MyController myController = (MyController) ctx.getBean("myController");
-
-		String greeting = myController.sayHello();
-
-		System.out.println(greeting);
-
-		System.out.println("--- Property based DI ---");
-
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
-		System.out.println(propertyInjectedController.getGreeting());
-
-		System.out.println("--- Setter based DI ---");
-
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-
-		System.out.println(setterInjectedController.getGreeting());
-
-		System.out.println("--- Constructor based DI ---");
-
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 
-		System.out.println(constructorInjectedController.getGreeting());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+		System.out.println(fakeDataSource.getUrl());
 
 	}
 
